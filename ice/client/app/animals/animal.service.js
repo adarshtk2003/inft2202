2227a9b27783d13e53814d05002293fa24fdabf5
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2025-01-24 22:28:26 -0500

    Creating both animal.html and index.js

--- ice/client/app/animals/animal.service.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Adarsh Thekkekulathingal Kuriyachan
-    filename: animal.service.js
-    Course: INFT 2202
-    Date: January 10, 2025
-    Description: This is my general application script.  Functions that are required on every page should live here.
-*/
