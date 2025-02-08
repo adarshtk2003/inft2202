720b6926b831f2f923d777b38b2ad0741a03a1be
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2025-02-08 00:08:04 -0500

    .

--- ice/client/app/create.js
+++ ice/client/app/create.js
@@ -1,28 +1,28 @@
-// tell us the page
+// tell us what page we're on
 console.log('we are on the add page');
 
-// assign a handler
+// assign a handler to the submit event
 document.getElementById('animal-form')
     .addEventListener('submit', submitAnimalForm);
 
-// create a handler to deal with submit event
+// create a handler to deal with the submit event
 async function submitAnimalForm ( event ) {
     // prevent the default action from happening
     event.preventDefault();
-    // get a reference to the form
+    // get a reference to the form (from the event)
     const animalForm = event.target;  
     // validate the form
     const valid = validateAnimalForm(animalForm);
-    // proceed if the form is valid
+    // do stuff if the form is valid
     if (valid) {
         console.log('were good');
         
         const formData = new FormData(animalForm);
-        //creating javascript object to hold the form data
+        //create a javascript object to hold the form data
         const animalObject = {};
         formData.forEach((value, key) => {
-
-            // form is string 
+            //by default, a value from form is string
+            //we need to convert them accordingly
             if(key === 'eyes' || key ==='legs'){
                 animalObject[key] = Number(value);
             }
@@ -42,17 +42,17 @@ async function submitAnimalForm ( event ) {
             eleNameError.classList.remove('d-none');
             eleNameError.textContent = "This animal already exists!";
         }        
-
+    // do nothing if it's not
     } else {
         console.log('were not good');
     }
 }
 
-// animal form validation
+// validate the animal form
 function validateAnimalForm ( form ) {
     console.log('validating')
     let valid = true;
-    
+    // test that name is valid
     const name = form.name.value;
     const eleNameError = form.name.nextElementSibling
     if (name == "") {
@@ -62,7 +62,8 @@ function validateAnimalForm ( form ) {
     } else {
         eleNameError.classList.add('d-none');
     }
+    // add validation for the remaining fields. 
 
-    // add validation 
+    // return if the form is valid or not
     return valid
 }
