46e4a76ceaea95a989840d69576fe4e7feb8ba2b
Adarsh Thekkekulathingal Kuriyachan <adarsh.thekkekulathingalkuriya@dcmail.ca>
2025-03-10 18:06:44 -0400

    assignment 2

--- assignment1/client/app/products/product.js
+++ assignment1/client/app/products/product.js
@@ -4,16 +4,15 @@
 // File: product.js
 // Description: This file defines the Product constructor function for the product shop.
 
-function Product(name, description, stock, price) {
-    // Generate a unique ID for each product using a random string
-    this.id = Math.random().toString(36).substr(2, 9);
-    
-    // Assign provided values to product properties
+// client/app/products/product.js
+function Product(name, description, stock, price, owner = null) {
+    this.id = Math.random().toString(36).substr(2, 9); // Generate a unique ID
     this.name = name;
     this.description = description;
     this.stock = stock;
     this.price = price;
+    this.owner = owner;
+    this.createdAt = new Date().toISOString();
 }
 
-// Export the Product constructor for use in other modules
 export default Product;
